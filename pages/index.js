6ad89810f78df397b5cd576996f3e5ff2fa3d5ab
minooo <552594996@qq.com>
2018-01-22 12:50:01 +0800

    base

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Carousel } from "antd";
-import uuid from "uuid/v4";
 import reduxPage from "@reduxPage";
-import { Layout, HomeType } from "@components";
+import { Layout } from "@components";
 
 @reduxPage
 @connect(({ home }) => ({ home }))
 export default class extends Component {
-  state = {
-    typeList: [
-      {
-        title: "标题1",
-        caption: "副标题",
-        img: "http://dummyimage.com/100x100"
-      },
-      {
-        title: "标题1",
-        caption: "副标题",
-        img: "http://dummyimage.com/100x100"
-      },
-      {
-        title: "标题1",
-        caption: "副标题",
-        img: "http://dummyimage.com/100x100"
-      },
-      {
-        title: "标题1",
-        caption: "副标题",
-        img: "http://dummyimage.com/100x100"
-      },
-      {
-        title: "标题1",
-        caption: "副标题",
-        img: "http://dummyimage.com/100x100"
-      }
-    ]
-  };
+  state = {};
   render() {
-    const { typeList } = this.state;
     return (
       <Layout title="首页">
-        <Carousel afterChange={e => console.info(e)} className="home-carousel">
-          <div>
-            <h3 className="home-slide">aaa</h3>
-          </div>
-          <div>
-            <h3 className="home-slide">bbb</h3>
-          </div>
-          <div>
-            <h3 className="home-slide">ccc</h3>
-          </div>
-        </Carousel>
-        <div className="flex jc-between bg-white box">
-          {typeList.map(item => <HomeType key={uuid()} item={item} />)}
-        </div>
+        首页
       </Layout>
     );
   }
