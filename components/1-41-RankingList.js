fed17e60fedc25355f49f67e01ef753808a71c96
duzhihao <du@cc.com>
2018-01-23 14:23:09 +0800

    14.23

--- components/1-41-RankingList.js
+++ components/1-41-RankingList.js
@@ -1,42 +1,72 @@
 import uuid from "uuid/v4";
 import { Icon } from "antd";
+import React from "react";
+import { Btn, WrapLink } from "@components";
 
-export default ({ rankingList }) => (
-  <div
-    className="plr20 pt20 bg-white font14"
-    style={{ height: "520px" }}
-  >
-    <div className="flex jc-between ai-center mb30">
-      <div className="font22 c333">{rankingList.type}</div>
-      <div className="font16 c666">
-        <span className="mr5">{rankingList.new}</span>
-        <span>|</span>
-        <span className="ml5">{rankingList.hot}</span>
-      </div>
-    </div>
-    {rankingList.list.map(item => (
-      <div key={uuid()} className="flex mb10 pl10 pt20 pr20" style={{ width: "300px", height: "100px" }}>
-        <div>
-          <img
-            src={item.img}
-            alt=""
-          />
-        </div>
-        <div className="flex equal jc-between pl15">
-          <div className="flex column jc-around c333 font12">
-            <div className="flex font16">
-              {item.title}
-            </div>
-            <div>
-              <span className="c-main">{item.number}</span>申请
-            </div>
-            <div className=" font12">{item.content}</div>
-          </div>
-          <div className="flex ai-center">
-            <Icon type="right" style={{ fontSize: 14 }} />
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handClickNew = this.handClickNew.bind(this)
+    this.handClickHot = this.handClickHot.bind(this)
+    this.state = { type: "hot" };
+  }
+  handClickNew() {
+    this.setState({
+      type: "new"
+    })
+  }
+  handClickHot() {
+    this.setState({
+      type: "hot"
+    })
+  }
+  render() {
+    const { rankingList } = this.props
+    const { type } = this.state
+    return (
+      <div
+        className="plr20 pt20 bg-white font14"
+        style={{ height: "520px" }}
+      >
+        <div className="flex jc-between ai-center">
+          <div className="font22 c333 lh100">{rankingList.type}</div>
+          <div className="font16 c666 flex">
+            <Btn onClick={this.handClickNew} con={<span className={`mr5 pointer ${type === "new" ? "c-main" : "c333"}`}>{rankingList.new}</span>} />
+            <span>|</span>
+            <Btn onClick={this.handClickHot} con={<span className={`ml5 pointer ${type === "hot" ? "c-main" : "c333"}`}>{rankingList.hot}</span>} />
           </div>
         </div>
+        <div className="h20 mb10 home-loan-list" />
+
+        {rankingList.list.map(item => (
+          <WrapLink key={uuid()}>
+            <div className="flex mb10 pl10 pt20 pb15 pr20 h100" style={{ width: "300px" }}>
+              <div>
+                <img
+                  src={item.img}
+                  alt=""
+                />
+              </div>
+              <div className="flex equal jc-between pl15">
+                <div className="flex column jc-between c333 font12">
+                  <div className="flex font16 lh100">
+                    {item.title}
+                  </div>
+                  <div>
+                    <span className="c-main">{item.number}</span>申请
+                  </div>
+                  <div className="font12 lh100">{item.content}</div>
+                </div>
+                <div className="flex ai-center">
+                  <Icon type="right" style={{ fontSize: 14 }} />
+                </div>
+              </div>
+            </div>
+          </WrapLink>
+        ))
+        }
       </div>
-    ))}
-  </div>
-);
+
+    )
+  }
+}
--- components/1-42-OnlineLoans.js
+++ components/1-42-OnlineLoans.js
@@ -1,73 +1,100 @@
 import { HomeRankingList, Btn } from "@components";
 import { Carousel, Icon } from "antd"
 import uuid from "uuid/v4";
+import React from "react";
 
-export default ({ rankingList, onlineLoans }) => (
-  <div className="box flex lh120" style={{ marginBottom: "100px" }}>
-    <div className="mt20 pt25 pl20 pr15 c333 mb20 bg-white" style={{ width: "860px", height: "480px" }}>
-      <div className="flex jc-between mb20">
-        <div className="font20 c333">{onlineLoans.type}</div>
-        <div className="flex ai-center">
-          <span className="pr10">扫码进入手机版</span>
-          <img src={onlineLoans.qrcode} style={{ width: "22px", height: "22px" }} alt="" />
-        </div>
-      </div>
-      <div className="flex">
-        <div className="pr30">
-          <div className="mb20 overflow-h" style={{ width: "200px", height: "125px" }}>
-            <Carousel autoplay >
-              {onlineLoans.carouselList &&
-                onlineLoans.carouselList.length > 0 && (
-                  onlineLoans.carouselList.map((src) => <img src={src} alt="" />)
-                )}
-            </Carousel>
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handOver = this.handOver.bind(this)
+    this.handOut = this.handOut.bind(this)
+    this.state = { isOver: false };
+  }
+
+  handOver() {
+    this.setState({
+      isOver: true
+    })
+  }
+  handOut() {
+    this.setState({
+      isOver: false
+    })
+  }
+  render() {
+    const { rankingList, onlineLoans } = this.props
+    const { isOver } = this.state
+    return (
+      <div className="box flex lh120" style={{ marginBottom: "100px" }}>
+        <div className="mt20 pt25 pl20 pr15 c333 mb20 bg-white" style={{ width: "860px", height: "480px" }}>
+          <div className="flex jc-between mb20">
+            <div className="font20 c333">{onlineLoans.type}</div>
+            <div className="flex ai-center relative" onMouseEnter={this.handOver} onMouseLeave={this.handOut}>
+              <span className="pr10">扫码进入手机版</span>
+              <Icon type="qrcode" style={{ fontSize: 22 }} />
+              { isOver ? <img className="home-online-pctop" src="https://dummyimage.com/110x110" alt="" style={{ width: "110px", position: "absolute", top: "40px" }} /> : null }
+            </div>
           </div>
-          <div className="text-center mb20">
-            <div className="font16 mb10">{onlineLoans.num}</div>
-            <div className="font14">{onlineLoans.type2}</div>
-          </div>
-          <div className="text-center mb20">
-            <Btn className="inline-block bg-main r12" style={{ width: "140px", height: "35px" }} />
-          </div>
-          <div className="text-center font14 mb20">
-            <p>APP下载，享专属优惠</p>
-          </div>
-          <div className="flex jc-between ai-center">
-            <img src="https://dummyimage.com/80x115" style={{ width: "80px", height: "115px" }} alt="" />
-            <img src="https://dummyimage.com/90x90" alt="" style={{ width: "90px", height: "90px" }} />
-          </div>
-        </div>
-        <div className="flex wrap pl5">
-          {onlineLoans.list.map((item) => (
-            <div key={uuid()} className="flex mb10 pl10 pt20 pr15" style={{ width: "295px", height: "100px" }}>
-              <div>
-                <img
-                  src={item.img}
-                  alt=""
-                />
+          <div className="flex">
+            <div className="pr30">
+              <div className="mb20 overflow-h" style={{ width: "200px", height: "125px" }}>
+                <Carousel autoplay >
+                  {onlineLoans.carouselList &&
+                    onlineLoans.carouselList.length > 0 && (
+                      onlineLoans.carouselList.map((src) => <img key={uuid()} src={src} alt="" />)
+                    )}
+                </Carousel>
               </div>
-              <div className="flex equal jc-between pl15">
-                <div className="flex column jc-around c333 font12">
-                  <div className="flex font16">
-                    {item.title}
-                  </div>
-                  <div>申请</div>
-                  <div className="font12 c-second">{item.content}</div>
-                </div>
-                <div className="flex ai-center">
-                  <Icon type="right" style={{ fontSize: 14 }} />
-                </div>
+              <div className="text-center mb20">
+                <div className="font16 mb10">{onlineLoans.num}</div>
+                <div className="font14">{onlineLoans.type2}</div>
+              </div>
+              <div className="flex jc-center mb20">
+                <Btn con={<span className="block font18 c-white lh150">免费申请</span>} className="flex jc-center ai-center bg-main r12 h36" style={{ width: "140px" }} />
+              </div>
+              <div className="text-center font14 mb20">
+                <p>APP下载，享专属优惠</p>
+              </div>
+              <div className="flex jc-between ai-center">
+                <img src="https://dummyimage.com/80x115" style={{ width: "80px", height: "115px" }} alt="" />
+                <img src="https://dummyimage.com/90x90" alt="" style={{ width: "90px", height: "90px" }} />
               </div>
             </div>
-          ))}
+            <div className="flex wrap pl5">
+              {onlineLoans.list.map((item) => (
+                <div key={uuid()} className="flex mb10 pl10 pt20 pr15" style={{ width: "295px", height: "100px" }}>
+                  <div>
+                    <img
+                      src={item.img}
+                      alt=""
+                    />
+                  </div>
+                  <div className="flex equal jc-between pl15">
+                    <div className="flex column jc-around c333 font12">
+                      <div className="flex font16">
+                        {item.title}
+                      </div>
+                      <div>申请</div>
+                      <div className="font12 c-second">{item.content}</div>
+                    </div>
+                    <div className="flex ai-center">
+                      <Icon type="right" style={{ fontSize: 14 }} />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div>
+          {rankingList &&
+            rankingList.list.length > 0 && (
+              <HomeRankingList rankingList={rankingList} />
+            )}
         </div>
       </div>
-    </div>
-    <div>
-      {rankingList &&
-        rankingList.list.length > 0 && (
-          <HomeRankingList rankingList={rankingList} />
-        )}
-    </div>
-  </div>
-)
+    )
+  }
+}
+
