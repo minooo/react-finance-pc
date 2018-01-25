6047fd3baa6b19df7dc6ab1eae1c1e063bec4d01
liushuaibao <1447792802@qq.com>
2018-01-25 17:43:34 +0800

    修复字体bug

--- components/1-06-HotNew.js
+++ components/1-06-HotNew.js
@@ -1,14 +1,18 @@
 import uuid from "uuid/v4";
 import { WrapLink } from "@components";
 
-export default ({ item }) => (
+export default ({ item, index }) => (
   <div style={{ width: "224px" }}>
     <div style={{ height: "140px" }} className="img-bg home-shdow-sm mb10">
-      <img src={item.img} className="w-100 h-100" alt="" />
+      <img
+        src={`/static/images/home_hotnew_${index}.png`}
+        className="w-100 h-100"
+        alt=""
+      />
     </div>
-    {item.list &&
-      item.list.length > 0 &&
-      item.list.map(n => (
+    {item &&
+      item.length > 0 &&
+      item.map(n => (
         <WrapLink
           key={uuid()}
           href="/index"
