40e21e8947b369bebe653fa0388556326fc23404
liushuaibao <1447792802@qq.com>
2018-01-26 16:08:54 +0800

    微调

--- components/4-01-NewList.js
+++ components/4-01-NewList.js
@@ -10,14 +10,14 @@ export default ({ item }) => (
     <div
       style={{
         width: "176px",
-        top: -20,
+        top: -10,
         left: -20
       }}
       className="img-bg h110 relative new-shadow"
     >
       <img className="w-100 h-100" src={item.img} alt="" />
     </div>
-    <div style={{ width: "642px" }} className="flex column jc-between ptb15">
+    <div style={{ width: "642px" }} className="flex h120 column jc-between ptb15">
       <div className="font18 bold  text-overflow-one">{item.title}</div>
       <div className="font14  text-overflow-one">{item.text}</div>
       <div className="flex c666 jc-end new-color">
