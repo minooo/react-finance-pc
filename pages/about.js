9704c2542e79afd063b08e43798cfdd784e71ba6
liushuaibao <1447792802@qq.com>
2018-01-27 17:08:57 +0800

    关于我们

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,14 @@ import { Layout } from "@components";
 export default class extends Component {
   state = {};
   render() {
-    return <Layout title="关于我们">关于我们</Layout>;
+    return (
+      <Layout title="关于我们" style={{ backgroundColor: "#fff" }}>
+        {/* banan */}
+        <div
+          style={{ height: "300px", backgroundColor: "#88bdff" }}
+          className="about-banner-bg"
+        />
+      </Layout>
+    );
   }
 }
