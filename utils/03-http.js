1aa0f9210c12ac977343cca958575fe6dc6f917d
minooo <552594996@qq.com>
2018-01-30 16:12:20 +0800

    start-test 本地配置优化

--- utils/03-http.js
+++ utils/03-http.js
@@ -12,13 +12,15 @@ import axios from "axios";
 // baseURL: ctx.isServer ? 'http://m.jrdudu.com/api/' : '/api/',
 // const dev = process.env.NODE_ENV !== 'production'
 // const baseURL = dev ? 'http://m.jrdudu.com/api/' : '/api/'
+const pro = process.env.NODE_ENV === "production"
+const test = process.env.NODE_TEST === "test"
 const callApi = (url, method, data, isServer = false, options = {}) => {
   const opts = { ...options };
   return axios(
     Object.assign(
       {},
       {
-        baseURL: isServer ? "http://jr.duduapp.net/web/" : "/web/",
+        baseURL: isServer ? ((pro && !test) ? "http://jr.duduapp.net/web/" : "http://192.168.1.116/web/") : "/web/",
         url,
         method,
         params: method === "get" ? data : {}, // 添加在请求URL后面的参数
