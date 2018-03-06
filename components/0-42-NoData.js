07ec230981fdec8a325b91328b1c9d563f03abd1
e <1447792802@qq.com>
2018-03-06 10:23:32 +0800

    sm

--- components/0-42-NoData.js
+++ components/0-42-NoData.js
@@ -1,6 +1,6 @@
-export default ({ title, caption }) => (
+export default ({ title, caption, ico = "me-nodata-bg" }) => (
   <div className="flex ai-center jc-center column w-100 h-100">
-    <div className="me-nodata-bg" />
+    <div className={ico} />
     {title && <div className="font22 mb10 pb5 c333">{title}</div>}
     <div className="font16 c999">{caption || "暂无相关数据"}</div>
   </div>
