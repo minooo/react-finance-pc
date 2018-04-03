833d2114595bff5d91f0bc3021e295374d51087d
minooo <552594996@qq.com>
2018-04-03 10:31:10 +0800

    opt

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
           <meta name="description" content="嘟嘟e贷，一站式专业贷款服务平台！" />
           <link rel="icon" href="http://public.duduapp.net/finance/pc-static/img/favicon.ico" type="image/x-icon" />
           <link rel="stylesheet" href={`${path}/static/styles/antd_min.css?4`} />
-          <link rel="stylesheet" href={`${path}/static/styles/app_min.css?4`} />
+          <link rel="stylesheet" href={`${path}/static/styles/app_min.css?5`} />
           <title>嘟嘟e贷</title>
         </Head>
         <body>
